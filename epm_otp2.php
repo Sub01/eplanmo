34c76d7a65916d798aa897bd5d39b8e48eaad4fa
GINO <you@example.com>
2021-11-12 11:53:25 +0800

    make it better

---
+++ epm_otp2.php
@@ -0,0 +1,45 @@
+<?php 
+
+$sql1 = "SELECT * FROM users WHERE Email='$email'";
+$result = $db-> query($sql);
+if ($result-> num_rows >0) {
+    $row = mysqli_fetch_assoc($result);
+		
+	$name = $row['Name'];
+	$surname = $row['Surname'];
+	$code = $row['Code'];
+		
+	//==========================  INSTANTIATE MAILER
+	$mail = new PHPMailer(true);
+	$mail->isHTML(true);
+	$mail->isSMTP();
+	$mail->CharSet = "utf-8";
+		
+	//==========================  GOOGLE ACCOUNT CREDENTIALS
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = "true";
+	$mail->Username = "mh.tokio@gmail.com";
+	$mail->Password = "cwovxtcdrzoxjmmp";
+	$mail->SMTPSecure = "ssl";
+	$mail->Port = "465";
+		
+	//==========================  EMAIL INFORMATIONS
+	$mail->setFrom("mh.tokio@gmail.com","E-Plan Mo");
+	$newname = $name. " " .$surname;
+	$mail->addAddress("$email", $name);
+		
+	$email_template = 'epm_mail_template.html';
+	$message = file_get_contents($email_template);
+	$message = str_replace('%user%', $newname, $message);
+	$message = str_replace('%code%', $code, $message);
+		
+	$mail->msgHTML($message);
+		
+	if($mail->send()){
+		$script = "<script> $(document).ready(function(){ $('#modalResendSuccess').modal('show'); }); </script>";
+	}else{
+		$script = "<script> $(document).ready(function(){ $('#modalResendFailed').modal('show'); }); </script>";
+	}
+}
+
+?>
