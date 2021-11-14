70efa3a633c47931c4c9ab7554ac49fc014441e4
GINO <you@example.com>
2021-11-14 17:19:27 +0800

    fix timer for otp expiration

--- epm_otp.php
+++ epm_otp.php
@@ -24,92 +24,84 @@ else{
 	$fetch = mysqli_fetch_assoc($result);
 	$time = $fetch['Timer'];
 	$cstts = $fetch['Code_Status'];
-	if($result){
-		if(isset($_POST['submit'])) {
-			$sql2 = "UPDATE `users` SET Code='0', Status='Verified' WHERE Email='$email'";
-			$result2 = $db-> query($sql2);
-			if($result2){
-				if($cstts=="Valid"){
-					$_SESSION['status'] = "success";
-					$_SESSION['message'] = "EMAIL VERIFICATION COMPLETE";
-					header('Location: index.php');
-					exit();
-				}
-				else{
-					$_SESSION['status'] = "error";
-					$_SESSION['message'] = "YOU'VE ENTERED A EXPIRED OTP";
-					header("Location: epm_otp.php?email=$email");
-					exit();
-				}
-				if(isset($_POST['resend'])) {
-					$sql = "SELECT * FROM users WHERE Email='$email'";
-					$result = $db-> query($sql);
-					if ($result-> num_rows >0) {
-    					$row = mysqli_fetch_assoc($result);
-						$code = rand(999999, 111111);
-      					$status = "Unverified";
-						$name = $row['Name'];
-						$surname = $row['Surname'];
-						$code_status = "Valid";
-						$timer = 120;
-		
-			//==========================  INSTANTIATE MAILER
-						$mail = new PHPMailer(true);
-						$mail->isHTML(true);
-						$mail->isSMTP();
-						$mail->CharSet = "utf-8";
-		
-			//==========================  GOOGLE ACCOUNT CREDENTIALS
-						$mail->Host = 'smtp.gmail.com';
-						$mail->SMTPAuth = "true";
-						$mail->Username = "mh.tokio@gmail.com";
-						$mail->Password = "cwovxtcdrzoxjmmp";
-						$mail->SMTPSecure = "ssl";
-						$mail->Port = "465";
-		
-			//==========================  EMAIL INFORMATIONS
-						$mail->setFrom("mh.tokio@gmail.com","E-Plan Mo");
-						$newname = $name. " " .$surname;
-						$mail->addAddress("$email", $name);
-		
-						$email_template = 'epm_mail_template.html';
-						$message = file_get_contents($email_template);
-						$message = str_replace('%user%', $newname, $message);
-						$message = str_replace('%code%', $code, $message);
-		
-						$mail->msgHTML($message);
-		
-						if($mail->send()){
-							$sql = "UPDATE users SET Code='$code', Code_Status='$code_status', Timer='$timer' WHERE email='$email'";
-							$result = $db-> query($sql);
-							if($result){
-								$script = "<script> $(document).ready(function(){ $('#modalResendSuccess').modal('show'); }); </script>";
-							}
-							else{
-								$script = "<script> $(document).ready(function(){ $('#modalResendFailed').modal('show'); }); </script>";
-							}
-						}else{
-							$script = "<script> $(document).ready(function(){ $('#modalResendFailed').modal('show'); }); </script>";
-						}
-					}
-				}
-				if(isset($_POST['back'])){
-					unset($_SESSION['email']);
-					header('Location: index.php');
-					exit();
-				}
+	if(isset($_POST['submit'])) {
+		$sql2 = "UPDATE `users` SET Code='0', Status='Verified' WHERE Email='$email'";
+		$result2 = $db-> query($sql2);
+		if($result2){
+			if($cstts=="Valid"){
+				$_SESSION['status'] = "success";
+				$_SESSION['message'] = "EMAIL VERIFICATION COMPLETE";
+				header('Location: index.php');
+				exit();
 			}
 			else{
 				$_SESSION['status'] = "error";
-				$_SESSION['message'] = "YOU'VE ENTERED A WRONG OTP";
+				$_SESSION['message'] = "YOU'VE ENTERED A EXPIRED OTP";
 				header("Location: epm_otp.php?email=$email");
 				exit();
 			}
 		}
+		else{
+			$_SESSION['status'] = "error";
+			$_SESSION['message'] = "YOU'VE ENTERED A WRONG OTP";
+			header("Location: epm_otp.php?email=$email");
+			exit();
+		}
 	}
-	else{
-		$_SESSION['status'] = "error";
-		$_SESSION['message'] = "SESSION TIMEOUT";
+	if(isset($_POST['resend'])) {
+		$sql = "SELECT * FROM users WHERE Email='$email'";
+		$result = $db-> query($sql);
+		if ($result-> num_rows >0) {
+    		$row = mysqli_fetch_assoc($result);
+			$code = rand(999999, 111111);
+      		$status = "Unverified";
+			$name = $row['Name'];
+			$surname = $row['Surname'];
+			$code_status = "Valid";
+			$timer = 120;
+		
+			//==========================  INSTANTIATE MAILER
+			$mail = new PHPMailer(true);
+			$mail->isHTML(true);
+			$mail->isSMTP();
+			$mail->CharSet = "utf-8";
+		
+			//==========================  GOOGLE ACCOUNT CREDENTIALS
+			$mail->Host = 'smtp.gmail.com';
+			$mail->SMTPAuth = "true";
+			$mail->Username = "mh.tokio@gmail.com";
+			$mail->Password = "cwovxtcdrzoxjmmp";
+			$mail->SMTPSecure = "ssl";
+			$mail->Port = "465";
+		
+			//==========================  EMAIL INFORMATIONS
+			$mail->setFrom("mh.tokio@gmail.com","E-Plan Mo");
+			$newname = $name. " " .$surname;
+			$mail->addAddress("$email", $name);
+		
+			$email_template = 'epm_mail_template.html';
+			$message = file_get_contents($email_template);
+			$message = str_replace('%user%', $newname, $message);
+			$message = str_replace('%code%', $code, $message);
+		
+			$mail->msgHTML($message);
+		
+			if($mail->send()){
+				$sql = "UPDATE users SET Code='$code', Code_Status='$code_status', Timer='$timer' WHERE email='$email'";
+				$result = $db-> query($sql);
+				if($result){
+					$script = "<script> $(document).ready(function(){ $('#modalResendSuccess').modal('show'); }); </script>";
+				}
+				else{
+					$script = "<script> $(document).ready(function(){ $('#modalResendFailed').modal('show'); }); </script>";
+				}
+			}else{
+				$script = "<script> $(document).ready(function(){ $('#modalResendFailed').modal('show'); }); </script>";
+			}
+		}
+	}
+	if(isset($_POST['back'])){
+		unset($_SESSION['email']);
 		header('Location: index.php');
 		exit();
 	}
