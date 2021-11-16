7341d1f06281152c78ed89341ce46b1ae3e5ca12
GINO <you@example.com>
2021-11-16 23:27:09 +0800

    adding function to Add Teacher Form

--- assets/php/summary.php
+++ assets/php/summary.php
@@ -9,7 +9,7 @@
 	$sql2 = "SELECT Count(`Name`) AS total2 FROM `events` WHERE `Name`='$name' AND `Status`='Ongoing'";
 	$sql3 = "SELECT Count(`Name`) AS total3 FROM `events` WHERE `Name`='$name' AND `Status`='Ended'";
 	$sql4 = "SELECT Count(`Name`) AS total4 FROM `events` WHERE `Name`='$name' AND `Status`='Archived'";
-	$sql5 = "SELECT Count(`T_Name`) AS total5 FROM `teachers` WHERE `Name`='$name'";
+	$sql5 = "SELECT Count(`T_Name`) AS total5 FROM `teachers` WHERE `User`='$name'";
 	$result1 = $db-> query($sql1);
 	$result2 = $db-> query($sql2);
 	$result3 = $db-> query($sql3);
