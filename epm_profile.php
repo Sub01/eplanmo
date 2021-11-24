78f62fa8d656a127e3e4e312735921cb6f283452
GINO <you@example.com>
2021-11-24 12:58:09 +0800

    make it better

--- epm_profile.php
+++ epm_profile.php
@@ -43,7 +43,7 @@ include 'assets/php/php_epm_genset.php';
             	<a href="epm_agenda.php"><span class="fa fa-calendar-check mr-3"></span> Agenda</a>
           	</li>
 			<li>
-            	<a href="#" onclick="window.open('https://drive.google.com/file/d/1TKJAF_timsl2lkTc4M6U-C3AJKNPnvi2/view?usp=sharing')"> <span><i class="fab fa-android mr-3"></i></span> Android Version</a>
+            	<a href="#" onclick="window.open('https://drive.google.com/file/d/1IzF4Zl4sONXmk1uT8XAYgAWV-o5brriu/view?usp=sharing')"> <span><i class="fab fa-android mr-3"></i></span> Android Version</a>
           	</li>
         </ul>
     </nav>
