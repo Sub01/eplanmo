639cbb4a8c692c05750875ea02dcb4b6574673bd
GINO <you@example.com>
2021-11-25 14:11:53 +0800

    make it better

--- epm_agenda.php
+++ epm_agenda.php
@@ -41,11 +41,12 @@ include ("assets/php/summary.php");
           	<li class="active">
             	<a href="epm_agenda.php"><span class="fa fa-calendar-check mr-3"></span> Agenda</a>
           	</li>
-<!--
+			<li class="active">
+            	<a href="epm_agenda.php"><span class="fa fa-calendar-check mr-3"></span> Pomodoro Technique</a>
+          	</li>
 			<li>
             	<a href="https://mega.nz/file/T3AhzCgZ#BfyRpaYACNhw8AceTwl76YdSw__jPAN70wuRHQUk9-8g"><span><i class="fab fa-android mr-3"></i></span> Android Version</a>
           	</li>
--->
         </ul>
     </nav>
     <div id="content" class="d-flex flex-column">
