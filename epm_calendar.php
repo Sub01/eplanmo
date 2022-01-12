707b18461b843fee7a3babca5c30edc7386fafd1
Gino <kirigayakazuto03@rocketmail.com>
2022-01-13 07:34:48 +0800

    make it better

--- epm_calendar.php
+++ epm_calendar.php
@@ -186,7 +186,7 @@ else{
                       </div>
 					
 				<div class="row">
-					<div class="col-md-4">
+					<div class="col-lg-4 col-md-4">
 						<div class="row">
 							<div class="col-xl-12 col-md-12 mb-4">
 								<div class="card border-left-primary shadow h-100">
@@ -224,7 +224,6 @@ else{
 								</div>
 							</div>
 						</div>
-					</div>
 					<div class="col-xl-8 col-md-8 mb-4">
 						<div class="card border-left-primary shadow h-100 py-2" style="max-height: 1000px;">
                     		<div class="card-body" id="calendar">
