44eac73491dbd666c31b8b066c699d02ae250459
Gino <kirigayakazuto03@rocketmail.com>
2022-01-13 06:31:35 +0800

    make it better

--- assets/php/delete_teachers.php
+++ assets/php/delete_teachers.php
@@ -1,11 +1,13 @@
 <?php
 include 'config.php';
 session_start();
-$id = $_GET['id'];
-$sql = "DELETE FROM Teachers WHERE ID='$id'";
-$result = $db ->query($sql);
-$_SESSION['status'] = "success";
-$_SESSION['message'] = "Teacher Successfully Deleted";
-header("Location: /epm_teachers.php");
-exit();
+if (isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "DELETE FROM Teachers WHERE ID='$id'";
+    $result = $db ->query($sql);
+    $_SESSION['status'] = "success";
+    $_SESSION['message'] = "Teacher Successfully Deleted";
+    header("Location: /epm_teachers.php");
+    exit();
+}
 ?>
