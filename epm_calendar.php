1d3ec389f5f1ae9eb98cedea2449ae186966e519
Gino <kirigayakazuto03@rocketmail.com>
2022-01-17 15:55:10 +0800

    make it better

--- epm_calendar.php
+++ epm_calendar.php
@@ -224,7 +224,10 @@ else{
 							</div>
 						</div>
 					<div class="col-xl-8 col-md-8 mb-4">
-						<div class="card border-left-primary shadow h-100 py-2" style="max-height: 1000px;" id="calendar">
+						<div class="card border-left-primary shadow h-100 py-2" style="max-height: 1000px;">
+                    		<div id="calendar">
+
+							</div>
 						</div>
 					</div>
 				</div>
