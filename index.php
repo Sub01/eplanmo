5e9475f4590c8c8d9121364f0443b1865f0fda24
Gino <kirigayakazuto03@rocketmail.com>
2022-01-22 13:47:15 +0800

    make it better

--- index.php
+++ index.php
@@ -122,9 +122,9 @@ elseif(isset($_POST['forgot'])){
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
     </ul>
-    <a class="nav-link" href="#" style="color:white">Tour</a>
-    <a class="nav-link" href="#" style="color:white">Blog</a>
-    <a class="nav-link" href="#" style="color:white">About</a>
+    <a href="#" style="color:white">Tour</a>
+    <a href="#" style="color:white">Blog</a>
+    <a href="#" style="color:white">About</a>
     <button class="btn btn-light my-2 my-sm-0" type="submit">SIGN IN</button>
     </div>
 </nav>
