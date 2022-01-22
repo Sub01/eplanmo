eadd2bea9101e0637b9b9afd57f963b623059a54
Gino <kirigayakazuto03@rocketmail.com>
2022-01-22 14:33:55 +0800

    make it better

--- index.php
+++ index.php
@@ -144,12 +144,12 @@ elseif(isset($_POST['forgot'])){
                     <h2 style="color: green;text-align:center;font-weight:bolder">Organize your classes, task and exams &amp; never forget a lecture or assignments again </h2>
                 </div>
                 <div class="row">
-                    <div class="col-lg-2 col-md-2 col-sm-2">
+                    <div class="col-lg-1 col-md-1 col-sm-1">
                     </div>
-                    <div class="col-lg-8 col-md-8 col-sm-8">
-                         <p style="color:gray; font-weight:bolder;font-size:18px; text-align:justify">It's time to say goodbye to your paper planner. EPLAN MO is everything your paper planner is and more. Schedules, grades, teachers, subjects ? EPLAN MO has it covered on all your devices. Oh, and did we mention it's free ?</p>
+                    <div class="col-lg-10 col-md-10 col-sm-10">
+                         <p style="color:gray; font-weight:bolder;font-size:15px; text-align:justify">It's time to say goodbye to your paper planner. EPLAN MO is everything your paper planner is and more. Schedules, grades, teachers, subjects ? EPLAN MO has it covered on all your devices. Oh, and did we mention it's free ?</p>
                     </div>
-                    <div class="col-lg-2 col-md-2 col-sm-2">
+                    <div class="col-lg-1 col-md-1 col-sm-1">
                     </div>
                 </div>
             </div>
