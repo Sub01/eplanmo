7b3b466b2d467dbb6cd3203cd3e5fcf66a94f8d9
Gino <kirigayakazuto03@rocketmail.com>
2022-01-23 15:05:40 +0800

    make it open

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 include ("assets/php/update_sms.php");
-include "config.php";
 include 'assets/php/php_epm_genset.php';
 require 'assets/mailer/Exception.php';
 require 'assets/mailer/PHPMailer.php';
