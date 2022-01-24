dc9e9deafbe092d8ebcb8a2fe2631049cf988611
Gino <kirigayakazuto03@rocketmail.com>
2022-01-24 21:53:54 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -400,11 +400,9 @@ else{
                 scales: {
                     scales: {
                         yAxes: [{
-                            ticks: {
-                                beginAtZero: true,
-                                precision: 0,
-                                callback: function(value) {if (value % 1 === 0) {return value;}}
-                            }
+                            beginAtZero: true,
+                            precision: 0,
+                            callback: function(value) {if (value % 1 === 0) {return value;}}
                         }],
                         xAxes: [{
                             autoskip: true,
