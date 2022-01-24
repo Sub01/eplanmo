7edcc961de7a79975d9aeb32e4afbc1279412934
Gino <kirigayakazuto03@rocketmail.com>
2022-01-24 18:35:30 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -364,8 +364,7 @@ if(!isset($_SESSION['User'])){
             });
         });
         window.onload = function() {
-
-            var ctx = document.getElementById("chartContainer").getContext("2d");
+            var ctx = document.getElementById("examChart").getContext("2d");
 
             var myChart = new Chart(ctx, {
                 type: 'line',
@@ -373,15 +372,18 @@ if(!isset($_SESSION['User'])){
                     labels: ["2015-03-15T13:03:00Z", "2015-03-25T13:02:00Z", "2015-04-25T14:12:00Z"],
                     datasets: [{
                         label: 'Demo',
-                        data: [<?php 
-			                 $user = $_SESSION['User'];
-			                 $sql2 = "SELECT Timestamp,COUNT(*) FROM `events` WHERE Name='$user' GROUP BY Timestamp";
-			                 $result2 =$db->query($sql2);
-                             while ($row = mysqli_fetch_array($result2)) {?> {
-                                x: <?php echo $row['Timestamp'] ?>,
-                                y: <?php echo $row['COUNT(*)'] ?>
+                        data: [{
+                                t: "2015-03-15T13:03:00Z",
+                                y: 12
                             },
-                            <?php } ?>
+                            {
+                                t: "2015-03-25T13:02:00Z",
+                                y: 21
+                            },
+                            {
+                                t: "2015-04-25T14:12:00Z",
+                                y: 32
+                            }
                         ],
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
