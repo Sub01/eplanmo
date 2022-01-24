cf1204a4535f1eb29ec9b08eeeae9965eae861c7
Gino <kirigayakazuto03@rocketmail.com>
2022-01-24 21:11:44 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -2,11 +2,30 @@
 include ("assets/php/php_epm_profile.php");
 include ("assets/php/php_epm_genset.php");
 include ("assets/php/summary.php");
-include ("assets/php/get_data.php");
 
 if(!isset($_SESSION['User'])){
 	header("Location: index.php");
 	exit();
+}
+else{
+    $user = $_SESSION['User']
+    $data1 = '';
+    $data2 = '';
+
+    //query to get data from the table
+    $sql = "SELECT  DATE_FORMAT(`Timestamp`,'%m-%d-%y') AS `Current`, COUNT(`ID`) AS `Total` FROM `events`  WHERE Name ='$user' GROUP BY `Current`";
+
+    $result = mysqli_query($mysqli, $sql);
+
+    //loop through the returned data
+    while ($row = mysqli_fetch_array($result)) {
+
+        $data1 = $data1 . '"'. $row['Current'].'",';
+        $data2 = $data2 . '"'. $row['Total'] .'",';
+    }
+
+    $data1 = trim($data1,",");
+    $data2 = trim($data2,",");
 }
 ?>
 <!doctype html>
@@ -365,7 +384,7 @@ if(!isset($_SESSION['User'])){
             });
         });
         window.onload = function() {
-            var ctx = document.getElementById("chart").getContext('2d');
+            var ctx = document.getElementById("myChart").getContext('2d');
                 var myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
