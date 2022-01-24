185e3f9e8b0a5cbbfbe18ce6f3ca8c5c80ce150c
Gino <kirigayakazuto03@rocketmail.com>
2022-01-24 21:12:52 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -11,19 +11,13 @@ else{
     $user = $_SESSION['User']
     $data1 = '';
     $data2 = '';
-
-    //query to get data from the table
     $sql = "SELECT  DATE_FORMAT(`Timestamp`,'%m-%d-%y') AS `Current`, COUNT(`ID`) AS `Total` FROM `events`  WHERE Name ='$user' GROUP BY `Current`";
-
-    $result = mysqli_query($mysqli, $sql);
-
-    //loop through the returned data
+    $result = mysqli_query($db, $sql);
     while ($row = mysqli_fetch_array($result)) {
 
         $data1 = $data1 . '"'. $row['Current'].'",';
         $data2 = $data2 . '"'. $row['Total'] .'",';
     }
-
     $data1 = trim($data1,",");
     $data2 = trim($data2,",");
 }
