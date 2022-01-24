f89808b02ffe543bab0552d7ff0f4febb083a92a
Gino <kirigayakazuto03@rocketmail.com>
2022-01-24 18:27:52 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -364,44 +364,59 @@ if(!isset($_SESSION['User'])){
             });
         });
         window.onload = function() {
-           
-        var ctx = document.getElementById("chartContainer").getContext("2d");
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: [
-                    <?php 
+
+            var ctx = document.getElementById("chartContainer").getContext("2d");
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: [
+                        <?php 
 			                 $user = $_SESSION['User'];
-			                 $sql2 = "SELECT DAY(Timestamp),COUNT(*) FROM `events` WHERE Name='$user' GROUP BY DAY(Timestamp) ";
+			                 $sql2 = "SELECT Timestamp, FROM `events` WHERE Name='$user'";
 			                 $result2 =$db->query($sql2);
-                             while ($row = mysqli_fetch_array($result2)) {?>
-                    "<?php echo $row['DAY(Timestamp)'] ?>",
-                    <?php } ?>
-                ],
-                datasets: [{
-                    label: 'Activities',
-                    data: [<?php 
-			                 $user = $_SESSION['User'];
-			                 $sql2 = "SELECT DAY(Timestamp),COUNT(*) FROM `events` WHERE Name='$user' GROUP BY DAY(Timestamp) ";
-			                 $result2 =$db->query($sql2);
-                             while ($row = mysqli_fetch_array($result2)) {?> {
-                             x: <?php echo $row['DAY(Timestamp)'] ?>,
-                             y: <?php echo $row['COUNT(*)'] ?>
-                        },
+                             while ($row = mysqli_fetch_array($result2)) {?> "<?php echo $row['DAY(Timestamp)'] ?>",
                         <?php } ?>
                     ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    xAxes: [{
-                        type: 'time',
-                        distribution: 'linear'
+                    datasets: [{
+                        label: 'Activities',
+                        data: [<?php 
+			                 $user = $_SESSION['User'];
+			                 $sql2 = "SELECT Timestamp,COUNT(*) FROM `events` WHERE Name='$user' GROUP BY Timestamp";
+			                 $result2 =$db->query($sql2);
+                             while ($row = mysqli_fetch_array($result2)) {?> {
+                                x: <?php echo $row['Timestamp'] ?>,
+                                y: <?php echo $row['COUNT(*)'] ?>
+                            },
+                            <?php } ?>
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
                     }]
+                },
+                options: {
+                    scales: {
+                        xAxes: [{
+                            type: 'time',
+                            distribution: 'linear'
+                        }]
+                    }
                 }
-            }
-        });
+            });
         }
 
     </script>
