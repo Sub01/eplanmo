6f58d5e3542072d0e78d4eb2be4a8f28ac243538
Gino <kirigayakazuto03@rocketmail.com>
2022-01-24 18:12:16 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -364,36 +364,46 @@ if(!isset($_SESSION['User'])){
             });
         });
         window.onload = function() {
-            var ctx = document.getElementById("chartContainer").getContext("2d");
-            var myChart = new Chart(ctx, {
-                title: {
-                    text: "ACTIVITY LOG PER DAY"
-                },
-                data: [{
-                    type: "<?php echo $gensetchart ?>",
-                    options: {
-                        scales: {
-                            xAxes: [{
-                                type: 'date',
-                            }]
-                        }
-                    },
-                    dataPoints: [{
-                        <?php 
-                        $user = $_SESSION['User'];
-			            $sql2 = "SELECT DAY(Timestamp),COUNT(*) FROM `events` WHERE Name='$user' GROUP BY DAY(Timestamp) ";
-			            $result2 =$db->query($sql2);
-                        while ($row = mysqli_fetch_array($result2)) {?> 
-                            x: <?php echo $row['DAY(Timestamp)'] ?>,
-                            y: <?php echo $row['COUNT(*)'] ?>
-                        <?php } ?>
+           
+        var ctx = document.getElementById("chartContainer").getContext("2d");
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ["2015-03-15T13:03:00Z", "2015-03-25T13:02:00Z", "2015-04-25T14:12:00Z"],
+                datasets: [{
+                    label: 'Demo',
+                    data: [<?php 
+			                 $user = $_SESSION['User'];
+			                 $sql2 = "SELECT DAY(Timestamp),COUNT(*) FROM `events` WHERE Name='$user' GROUP BY DAY(Timestamp) ";
+			                 $result2 =$db->query($sql2);
+                             while ($row = mysqli_fetch_array($result2)) {?> {
+                             x: <?php echo $row['DAY(Timestamp)'] ?>,
+                             y: <?php echo $row['COUNT(*)'] ?>
                         },
-                    ]
+                        <?php } ?>
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
                 }]
-            });
-
-            chart.render();
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        distribution: 'linear'
+                    }]
+                }
+            }
+        });
         }
+
     </script>
     </body>
 
