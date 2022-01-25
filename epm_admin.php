1b40975b7317fff4d63546db5f44272454b064cd
Gino <kirigayakazuto03@rocketmail.com>
2022-01-25 12:51:22 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -180,7 +180,7 @@ else{
                 </div>
                 <!-- Content Row -->
                 <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
                         <div class="row">
                             <div class="col-xl-6 col-md-6 mb-4">
                                 <div class="card border-left-secondary shadow h-100 py-2">
@@ -190,6 +190,10 @@ else{
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                        <div class="row">
                             <div class="col-xl-2 col-md-2 mb-4">
                                 <div class="card border-left-primary shadow h-100 py-2" style="border-left-style: solid; border-left-width: thick; border-left-color:rgb(0, 123, 255) ">
                                     <div class="card-body">
@@ -247,6 +251,9 @@ else{
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                        <div class="row">
+                        
                         </div>
                     </div>
                 </div>
