49eb63ac838acdb20494ca8c60884fb85a62cc3a
Gino <kirigayakazuto03@rocketmail.com>
2022-01-25 14:25:53 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -91,7 +91,7 @@ else{
                     <!--=============================================== Dropdown - Alerts ===============================================-->
                     <!--=================================================================================================================-->
                     <li class="nav-item dropdown no-arrow mx-1">
-                       <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <i class="fas fa-bell" style="font-size:18px;"></i></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <i class="fas fa-bell" style="font-size:18px;"></i></a>
                         <ul class="dropdown-menu"></ul>
                     </li>
 
@@ -388,6 +388,37 @@ else{
     <!--===============================================================================================-->
     <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     <script>
+        $(document).ready(function() {
+
+            function load_unseen_notification(view = '') {
+                $.ajax({
+                    url: "assets/php/notif_fetch.php",
+                    method: "POST",
+                    data: {
+                        view: view
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        $('.dropdown-menu').html(data.notification);
+                        if (data.unseen_notification > 0) {
+                            $('.count').html(data.unseen_notification);
+                        }
+                    }
+                });
+            }
+
+            load_unseen_notification();
+
+            $(document).on('click', '.dropdown-toggle', function() {
+                $('.count').html('');
+                load_unseen_notification('yes');
+            });
+
+            setInterval(function() {
+                load_unseen_notification();;
+            }, 5000);
+
+        });
         $(document).ready(function() {
             $('#table1').DataTable({
                 "autoWidth": true
