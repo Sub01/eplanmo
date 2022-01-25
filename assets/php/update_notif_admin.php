ebaaa6b45947ea92c63cc9d6c9e247d6b914af04
Gino <kirigayakazuto03@rocketmail.com>
2022-01-25 17:20:12 +0800

    make it better

--- assets/php/update_notif_admin.php
+++ assets/php/update_notif_admin.php
@@ -3,7 +3,7 @@ include "config.php";
 session_start();
 
 if(isset($_GET['id'])){
-    $id = $_GET['id']
+    $id = $_GET['id'];
     $sql = "UPDATE `notif` SET `Status`='1' WHERE ID='$id'";
     $result = $db->query($sql);
     if($result){
