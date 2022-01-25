9d85a18a42e3639d12a1c2df068bddc0d8325a59
Gino <kirigayakazuto03@rocketmail.com>
2022-01-25 15:19:27 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -90,11 +90,9 @@ else{
 
                     <!--=============================================== Dropdown - Alerts ===============================================-->
                     <!--=================================================================================================================-->
-                    <li class="dropdown nav-item no-arrow">
-                        <a href="#" class=" nav-link dropdown-toggle-notif dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> 
-                            <i class="fas fa-bell" style="font-size:18px;"></i>
-                        </a>
-                        <ul class="dropdown-menu-notif dropdown-menu"></ul>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-bell" style="font-size:18px;"></span></a>
+                        <ul class="dropdown-menu"></ul>
                     </li>
 
                     <div class="topbar-divider d-none d-sm-block"></div>
@@ -494,60 +492,57 @@ else{
 
     </script>
     </body>
+
 </html>
 <script>
-$(document).ready(function(){
- 
- function load_unseen_notification(view = '')
- {
-  $.ajax({
-   url:"fetch.php",
-   method:"POST",
-   data:{view:view},
-   dataType:"json",
-   success:function(data)
-   {
-    $('.dropdown-menu').html(data.notification);
-    if(data.unseen_notification > 0)
-    {
-     $('.count').html(data.unseen_notification);
-    }
-   }
-  });
- }
- 
- load_unseen_notification();
- 
- $('#comment_form').on('submit', function(event){
-  event.preventDefault();
-  if($('#subject').val() != '' && $('#comment').val() != '')
-  {
-   var form_data = $(this).serialize();
-   $.ajax({
-    url:"insert.php",
-    method:"POST",
-    data:form_data,
-    success:function(data)
-    {
-     $('#comment_form')[0].reset();
-     load_unseen_notification();
-    }
-   });
-  }
-  else
-  {
-   alert("Both Fields are Required");
-  }
- });
- 
- $(document).on('click', '.dropdown-toggle', function(){
-  $('.count').html('');
-  load_unseen_notification('yes');
- });
- 
- setInterval(function(){ 
-  load_unseen_notification();; 
- }, 5000);
- 
-});
+    $(document).ready(function() {
+
+        function load_unseen_notification(view = '') {
+            $.ajax({
+                url: "fetch.php",
+                method: "POST",
+                data: {
+                    view: view
+                },
+                dataType: "json",
+                success: function(data) {
+                    $('.dropdown-menu').html(data.notification);
+                    if (data.unseen_notification > 0) {
+                        $('.count').html(data.unseen_notification);
+                    }
+                }
+            });
+        }
+
+        load_unseen_notification();
+
+        $('#comment_form').on('submit', function(event) {
+            event.preventDefault();
+            if ($('#subject').val() != '' && $('#comment').val() != '') {
+                var form_data = $(this).serialize();
+                $.ajax({
+                    url: "insert.php",
+                    method: "POST",
+                    data: form_data,
+                    success: function(data) {
+                        $('#comment_form')[0].reset();
+                        load_unseen_notification();
+                    }
+                });
+            } else {
+                alert("Both Fields are Required");
+            }
+        });
+
+        $(document).on('click', '.dropdown-toggle', function() {
+            $('.count').html('');
+            load_unseen_notification('yes');
+        });
+
+        setInterval(function() {
+            load_unseen_notification();;
+        }, 5000);
+
+    });
+
 </script>
--- fetch.php
+++ fetch.php
@@ -1,45 +1,49 @@
 <?php
-include("config.php");
-session_start();
-$name = $_SESSION['User'];
-if(isset($_POST["view"]))
+include('config.php');
+
+
+if(isset($_POST['view'])){
+
+if($_POST["view"] != '')
 {
- if($_POST["view"] != '')
+    $update_query = "UPDATE notif SET Status ='1' WHERE Status='0'";
+    mysqli_query($db, $update_query);
+}
+$query = "SELECT * FROM notif ORDER BY ID DESC LIMIT 5";
+$result = mysqli_query($db, $query);
+$output = '';
+if(mysqli_num_rows($result) > 0)
+{
+ while($row = mysqli_fetch_array($result))
  {
-  $update_query = "UPDATE notif SET Status='1' WHERE Status='0' AND Name='$name'";
-  mysqli_query($db, $update_query);
- }
- $query = "SELECT * FROM notif ORDER BY ID DESC LIMIT 5";
- $result = mysqli_query($db, $query);
- $output = '';
- 
- if(mysqli_num_rows($result) > 0)
- {
-  while($row = mysqli_fetch_array($result))
-  {
    $output .= '
    <li>
-    <a href="#">
-     <strong>'.$row["Title"].'</strong><br />
-     <small><em>'.$row["Description"].'</em></small>
-    </a>
+   <a href="#">
+   <strong>'.$row["Title"].'</strong><br />
+   <small><em>'.$row["Description"].'</em></small>
+   </a>
    </li>
-   <li class="divider"></li>
    ';
-  }
+
  }
- else
- {
-  $output .= '<li><a href="#" class="text-bold text-italic">No Notification Found</a></li>';
- }
- 
- $query_1 = "SELECT * FROM notif WHERE Status='0' AND Name='$name'";
- $result_1 = mysqli_query($db, $query_1);
- $count = mysqli_num_rows($result_1);
- $data = array(
-  'notification'   => $output,
-  'unseen_notification' => $count
- );
- echo json_encode($data);
 }
+else{
+     $output .= '
+     <li><a href="#" class="text-bold text-italic">No Noti Found</a></li>';
+}
+
+
+
+$status_query = "SELECT * FROM notif WHERE Status='0'";
+$result_query = mysqli_query($db, $status_query);
+$count = mysqli_num_rows($result_query);
+$data = array(
+    'notification' => $output,
+    'unseen_notification'  => $count
+);
+
+echo json_encode($data);
+
+}
+
 ?>
