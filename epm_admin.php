86406847d78d324a42baa0add811f3b59387d5cc
Gino <kirigayakazuto03@rocketmail.com>
2022-01-25 15:39:13 +0800

    make it better

--- epm_admin.php
+++ epm_admin.php
@@ -407,6 +407,33 @@ else{
 
 
         $(document).ready(function() {
+            function load_unseen_notification(view = '') {
+                $.ajax({
+                    url: "fetch.php",
+                    method: "POST",
+                    data: {
+                        view: view
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        $('#dropdown-menu').html(data.notification);
+                        if (data.unseen_notification > 0) {
+                            $('.count').html(data.unseen_notification);
+                        }
+                    }
+                });
+            }
+
+            load_unseen_notification();
+
+            $(document).on('click', '#dropdown-toggle', function() {
+                $('.count').html('');
+                load_unseen_notification('yes');
+            });
+
+            setInterval(function() {
+                load_unseen_notification();;
+            }, 5000);
             $('.editbutton').on('click', function() {
                 $('#updateEvent').modal('show');
                 $tr = $(this).closest('tr');
@@ -494,37 +521,3 @@ else{
     </body>
 
 </html>
-<script>
-    $(document).ready(function() {
-
-        function load_unseen_notification(view = '') {
-            $.ajax({
-                url: "fetch.php",
-                method: "POST",
-                data: {
-                    view: view
-                },
-                dataType: "json",
-                success: function(data) {
-                    $('#dropdown-menu').html(data.notification);
-                    if (data.unseen_notification > 0) {
-                        $('.count').html(data.unseen_notification);
-                    }
-                }
-            });
-        }
-
-        load_unseen_notification();
-
-        $(document).on('click', '#dropdown-toggle', function() {
-            $('.count').html('');
-            load_unseen_notification('yes');
-        });
-
-        setInterval(function() {
-            load_unseen_notification();;
-        }, 5000);
-
-    });
-
-</script>
