4a0aaf06755825bba22eb352a7112477359ddb1d
Gino <kirigayakazuto03@rocketmail.com>
2022-01-26 15:49:42 +0800

    fix -> admin dashboard counter

--- epm_admin.php
+++ epm_admin.php
@@ -222,7 +222,7 @@ else{
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">
-                                                    ONGOING ACTIVITIES
+                                                    ONGOING
                                                 </div>
                                                 <div class="h5 mb-0 font-weight-bold text-gray-800">
                                                     <?php echo $ongoing ?>
@@ -257,15 +257,15 @@ else{
                         </div>
                         <div class="row">
                             <div class="col-xl-4 col-md-4 col-sm-4 mb-4">
-                                <div class="card border-left-primary shadow h-100 py-2" style="border-left-style: solid; border-left-width: thick; border-left-color:rgb(0, 123, 255) ">
+                                <div class="card border-left-warning shadow h-100 py-2" style="border-left-style: solid; border-left-width: thick; border-left-color:rgb(0, 123, 255) ">
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                    TOTAL ACTIVITIES
+                                                    TEACHERS
                                                 </div>
                                                 <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                                    <?php echo $all ?>
+                                                    <?php echo $teachers ?>
                                                 </div>
                                             </div>
                                             <div class="col-auto">
@@ -276,15 +276,15 @@ else{
                                 </div>
                             </div>
                             <div class="col-xl-4 col-md-4 col-sm-4 mb-4">
-                                <div class="card border-left-secondary shadow h-100 py-2" style="border-left-style: solid; border-left-width: thick; border-left-color:rgb(108, 117, 125)">
+                                <div class="card border-left-success shadow h-100 py-2" style="border-left-style: solid; border-left-width: thick; border-left-color:rgb(108, 117, 125)">
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">
-                                                    ONGOING ACTIVITIES
+                                                    SUBJECTS
                                                 </div>
                                                 <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                                    <?php echo $ongoing ?>
+                                                    <?php echo $subjects ?>
                                                 </div>
                                             </div>
                                             <div class="col-auto">
@@ -295,15 +295,15 @@ else{
                                 </div>
                             </div>
                             <div class="col-xl-4 col-md-4 col-sm-4 mb-4">
-                                <div class="card border-left-danger shadow h-100 py-2" style="border-left-style: solid; border-left-width: thick; border-left-color:rgb(220, 53, 69)">
+                                <div class="card border-left-dark shadow h-100 py-2" style="border-left-style: solid; border-left-width: thick; border-left-color:rgb(220, 53, 69)">
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                                    ENDED
+                                                    GRADES
                                                 </div>
                                                 <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                                    <?php echo $ended ?>
+                                                    <?php echo $grades ?>
                                                 </div>
                                             </div>
                                             <div class="col-auto">
