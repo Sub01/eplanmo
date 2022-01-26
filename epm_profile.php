a4a99a1553c1527fb5369a04c880b3af58c42488
Gino <kirigayakazuto03@rocketmail.com>
2022-01-26 10:31:26 +0800

    updted rel->stylesheets of top-navbar

--- epm_profile.php
+++ epm_profile.php
@@ -16,16 +16,19 @@ else{
 <head>
     <title>EPM PROFILE</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/animation.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!--===============================================================================================-->
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
-    <!--===============================================================================================-->
-    <!--===============================================================================================-->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-    <!--===============================================================================================-->
     <link rel="stylesheet" href="assets/css/dashboard_main.css">
+    <link rel="stylesheet" href="assets/css/animation.css">
+    <link rel="stylesheet" href="assets/css/summary.css">
+    <link rel="stylesheet" href="assets/css/chart.css">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/general.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1739375817606067" crossorigin="anonymous"></script>
 </head>
