590f71b9763b6bc11cf58df2c4a18e284ccdce12
Gino <kirigayakazuto03@rocketmail.com>
2022-01-26 10:26:13 +0800

    added redirect for notif->dowloads

---
+++ assets/php/update_notif_downloads.php
@@ -0,0 +1,18 @@
+<?php
+include "config.php";
+session_start();
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "UPDATE `notif` SET `Status`='1' WHERE ID='$id'";
+    $result = $db->query($sql); 
+    if($result){
+        header("Location: /epm_downloads.php");
+   	    exit();
+    }
+    else{
+        header("Location: /epm_downloads.php");
+   	    exit();
+    }
+}
+?>
