68a191943c4196c244c279e93d882a5634fd915e
Gino <kirigayakazuto03@rocketmail.com>
2022-01-26 09:10:51 +0800

    make it better

--- epm_downloads.php
+++ epm_downloads.php
@@ -8,31 +8,7 @@ if(!isset($_SESSION["User"])){
 	exit();
 }	
 else{
-    if (isset($_GET['id'])) {
-        $id = $_GET['id'];
-
-        $sql = "SELECT * FROM `uploads` WHERE ID='$id'";
-        $result = mysqli_query($db, $sql);
-
-        $row = mysqli_fetch_assoc($result);
-        $filepath = '/assets/uploads/' . $row['Name'];
-
-        if (file_exists($filepath)) {
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream';charset=UTF-8);
-            header('Content-Disposition: attachment; filename=' . basename($filepath));
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize('/assets/uploads/' . $row['Name']));
-            readfile('/assets/uploads/' . $row['Name']);
-            $newCount = $row['Downloads'] + 1;
-            $updateQuery = "UPDATE uploads SET Downloads='$newCount' WHERE ID='$id'";
-            mysqli_query($db, $updateQuery);
-            exit();
-        }
-
-    }
+    
 }
 
 ?>
