ca7f7fd5b00b1b7e856d50846bc1a80b9de72b21
Gino <kirigayakazuto03@rocketmail.com>
2022-01-28 23:24:50 +0800

    make it better

--- upload.php
+++ upload.php
@@ -1,19 +1,20 @@
 <?php 
 include 'config.php';
 if (isset($_POST['save'])) {
-	$location = "assets/uploads/";
-	$file_new_name = date("dmy") . time() . $_FILES["file"]["name"];
-	$file_name = $_FILES['file']["name"];
+    $file_name = $_FILES['file']["name"];
+    $file_type = $_FILES['file']["type"];
+    $file_size = $_FILES['file']['size'];
 	$file_temp = $_FILES['file']["tmp_name"];
-	$file_size = $_FILES['file']['size'];
+    $file_new_name = date("dmy") . time() . $_FILES["file"]["name"];
+    $file_store = "../assets/uploads/".$file_new_name;
 	if ($file_size > 10485760) {
 		echo "<script>alert('Woops! File is too big. Maximum file size allowed for upload 10 MB.')</script>";
 	} 
     else{
-		$sql = "INSERT INTO uploads (Name, New_Name, Size,Downloads)VALUES ('$file_name','$file_new_name','$file_size','0')";
+		$sql = "INSERT INTO uploads (Name, New_Name, Size,Type)VALUES ('$file_name','$file_new_name','$file_size','$file_type')";
 		$result = mysqli_query($db, $sql);
 		if ($result) {
-			move_uploaded_file($file_temp, $location . $file_new_name);
+			move_uploaded_file($file_temp,$file_store);
 			echo "<script>alert('Wow! File uploaded successfully.')</script>";
 		} 
         else{
@@ -25,6 +26,7 @@ if (isset($_POST['save'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>FILE UPLOADS</title>
     <meta charset="utf-8">
@@ -43,65 +45,97 @@ if (isset($_POST['save'])) {
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 </head>
 <style>
-form {
-    width: 30%;
-    margin: 100px auto;
-    padding: 30px;
-    border: 1px solid #555;
-}
-input {
-    width: 100%;
-    border: 1px solid #f1e1e1;
-    display: block;
-    padding: 5px 10px;
-}
-button {
-  border: none;
-  padding: 10px;
-  border-radius: 5px;
-}
-table {
-  width: 60%;
-  border-collapse: collapse;
-  margin: 100px auto;
-}
-th,
-td {
-  height: 50px;
-  vertical-align: center;
-  border: 1px solid black;
-}
-    </style>
-  <body>
-<header id="header" class="d-flex align-items-center" style="background-color:maroon">
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <span>
-                <h1 class="logo" style="color:white;">EPLAN MO</h1>
-            </span>
-            <button class="navbar-toggler btn btn-light" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" style="color: maroon !important">
-                <span class="navbar-toggler-icon" style="color: white;"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav mr-auto">
-                </ul>
-                <a class="nav-link" href="#hero" style="color:white">HOME</a>
-                <a class="nav-link" href="#featured-services" style="color:white">SERVICES</a>
-                <a class="nav-link" href="#about" style="color:white">ABOUT</a>
-                <a class="nav-link" href="#contact" style="color:white">CONTACT</a>
-                <a href="epm_login.php"><button class="btn btn-light my-2 my-sm-0">SIGN IN</button></a>
+    .row{
+        margin-top:10px;
+    }
+    .col-lg-3{
+        margin: 2px;
+        
+    }
+    .upload-apk{
+        border: 1px solid #555;
+    }
+    input {
+        width: 100%;
+        border: 1px solid #f1e1e1;
+        display: block;
+        padding: 5px 10px;
+    }
+
+    button {
+        border: none;
+        padding: 10px;
+        border-radius: 5px;
+    }
+
+    table {
+        width: 60%;
+        border-collapse: collapse;
+        margin: 100px auto;
+    }
+
+    th,
+    td {
+        height: 50px;
+        vertical-align: center;
+        border: 1px solid black;
+    }
+</style>
+
+<body>
+    <header id="header" class="d-flex align-items-center" style="background-color:maroon">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <span>
+                    <h1 class="logo" style="color:white;">EPLAN MO</h1>
+                </span>
+                <button class="navbar-toggler btn btn-light" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" style="color: maroon !important">
+                    <span class="navbar-toggler-icon" style="color: white;"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                    </ul>
+                    <!--<a class="nav-link" href="#hero" style="color:white">HOME</a>
+                    <a class="nav-link" href="#featured-services" style="color:white">SERVICES</a>
+                    <a class="nav-link" href="#about" style="color:white">ABOUT</a>
+                    <a class="nav-link" href="#contact" style="color:white">CONTACT</a>
+                    <a href="epm_login.php"><button class="btn btn-light my-2 my-sm-0">SIGN IN</button></a>-->
+                </div>
+            </nav>
+        </div>
+    </header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="container">
+                <h3>This feature supports localhost only , do not use in deployment (Apply HEROKU CLI Commands after uploading on localhost to apply on eplanmo.herokupp.com)</h3>
             </div>
-        </nav>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-3 upload-apk">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <h3>USE THIS TO UPLOAD APK</h3>
+                    <input type="file" name="file"> <br>
+                    <button type="submit" name="save" class="btn btn-dark form-control">UPLOAD</button>
+                </form>
+                <br>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 upload-apk">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <h3>USE THIS TO UPLOAD IMAGES</h3>
+                    <input type="file" name="file"> <br>
+                    <button type="submit" name="save" class="btn btn-dark form-control">UPLOAD</button>
+                </form>
+                <br>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 upload-apk">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <h3>USE THIS TO UPLOAD PDF</h3>
+                    <input type="file" name="file"> <br>
+                    <button type="submit" name="save" class="btn btn-dark form-control">UPLOAD</button>
+                </form>
+                <br>
+            </div>
+        </div>
     </div>
-</header>
-    <div class="container">
-      <div class="row">
-        <form action="" method="post" enctype="multipart/form-data" >
-          <h3>Upload File</h3>
-          <input type="file" name="file"> <br>
-          <button type="submit" name="save">upload</button>
-        </form>
-      </div>
-    </div>
-  </body>
+</body>
 </html>
