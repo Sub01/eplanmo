48c5ff09ab1b19209a5728e94eba7184d97f7f00
Gino <kirigayakazuto03@rocketmail.com>
2022-01-29 03:05:50 +0800

    make it better

--- assets/php/file_downloads.php
+++ assets/php/file_downloads.php
@@ -2,21 +2,18 @@
 if(isset($_GET['path'])){
     $file = $_GET['path'];
     $fileName = basename($file);
-$filePath = 'assets/uploads/'.$fileName;
-if(!empty($fileName) && file_exists($filePath)){
-    // Define headers
-    header("Cache-Control: public");
-    header("Content-Description: File Transfer");
-    header("Content-Disposition: attachment; filename=$fileName");
-    header("Content-Type: application/zip");
-    header("Content-Transfer-Encoding: binary");
-    
-    // Read the file
-    readfile($filePath);
-    exit;
-}else{
-    echo 'The file does not exist.';
-}
+    $filePath = 'assets/uploads/'.$fileName;
+    if(!empty($fileName) && file_exists($filePath)){
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+        header("Content-Disposition: attachment; filename=$fileName");
+        header("Content-Transfer-Encoding: binary");
+        readfile($filePath);
+        exit;
+    }
+    else{
+        echo 'The file does not exist.';
+    }
 }
 
 ?>
