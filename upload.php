5766baaaf2e11baf210bcee983f938f33c9c0aca
Gino <kirigayakazuto03@rocketmail.com>
2022-01-28 19:18:54 +0800

    update -> upload folder and files

--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 <?php 
 include 'config.php';
 if (isset($_POST['save'])) {
-	$location ="/assets/uploads/";
+	$location = "https://eplanmo.herokuapp.com/assets/uploads/";
 	$file_new_name = date("dmy") . time() . $_FILES["file"]["name"];
 	$file_name = $_FILES['file']["name"];
 	$file_temp = $_FILES['file']["tmp_name"];
-	$file_size = $_FILES['file']["size"];
+	$file_size = $_FILES['file']['size'];
 	if ($file_size > 10485760) {
 		echo "<script>alert('Woops! File is too big. Maximum file size allowed for upload 10 MB.')</script>";
 	} 
@@ -26,7 +26,7 @@ if (isset($_POST['save'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>UPLOAD FILES</title>
+    <title></title>
 </head>
 <style>
 form {
