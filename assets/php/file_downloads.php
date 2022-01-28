15d89b9e86f617da12ab7b1fcecd2e0c3dc5dfbd
Gino <kirigayakazuto03@rocketmail.com>
2022-01-29 02:26:02 +0800

    make it better

--- assets/php/file_downloads.php
+++ assets/php/file_downloads.php
@@ -1,11 +1,23 @@
 <?php
-define('DB_SERVER', 'mysql-57012-0.cloudclusters.net');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'izju8Wk1');
-define('DB_DATABASE', 'eplanmo');
-define('DB_PORT','15418');
+if(isset($_GET['link']))
+{
+    $file = $_GET['link'];
+    $dir = "assets/uploads/";
+    $file = $dir . $file;
 
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-
-
+    if (file_exists($file))
+    {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename='.basename($file));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        ob_clean();
+        flush();
+        readfile($file);
+        exit;
+    }
+}
 ?>
