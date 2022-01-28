a2b90883f8bcb96684055ecc41a7386e56c79534
Gino <kirigayakazuto03@rocketmail.com>
2022-01-29 02:33:47 +0800

    make it better

--- assets/php/file_downloads.php
+++ assets/php/file_downloads.php
@@ -1,24 +1,24 @@
 <?php
 if(isset($_GET['link']))
 {
-    $file = $_GET['link'];
-    $dir = "assets/uploads/";
-    $file = $dir . $file;
+    $var_1 = $_GET['link'];
 
-    if (file_exists($file))
+$dir = "assets/uploads/";
+$file = $dir . $var_1;
+
+if (file_exists($file))
     {
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename='.basename($file));
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($file));
-        ob_clean();
-        flush();
-        readfile($file);
-        header("Location: https://eplanmo.herokuapp.com/epm_downloads.php")
-        exit;
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
     }
-}
+} //- the missing closing brace
 ?>
